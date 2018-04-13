f9d70b0654cec184660b773451f11d8cf3e01608
nilchaos87 <justthebit@inorbit.com>
2018-04-13 12:53:53 -0700

    Dealing with receive address fragmentation

--- src/Data/Bitcoin.purs
+++ src/Data/Bitcoin.purs
@@ -11,16 +11,20 @@ foreign import deriveAddress ∷ String → Int → String
 type Wallet = { receiveAddress ∷ String, balance ∷ Int }
 
 fetchWallet ∷ ∀ e. String → Aff (ajax ∷ AJAX | e) Wallet
-fetchWallet = fetchWallet_ 0 0
+fetchWallet = fetchWallet_ 0 0 0 0
 
-fetchWallet_ ∷ ∀ e. Int → Int → String → Aff (ajax ∷ AJAX | e) Wallet
-fetchWallet_ balance n key = do
-  xhr ← get $ "https://blockchain.info/q/addressbalance/" <> receiveAddress
-  let additionalBalance = balanceFromString xhr.response
-  if (additionalBalance == 0) then (pure $ currentWallet) else (fetchWallet_ (balance + additionalBalance) (n + 1) key)
+fetchWallet_ ∷ ∀ e. Int → Int → Int → Int → String → Aff (ajax ∷ AJAX | e) Wallet
+fetchWallet_ misses balance lastHitN n key = do
+  xhr ← get $ "https://blockchain.info/q/addressbalance/" <> currentAddress
+  let currentBalance = balanceFromString xhr.response
+  let newBalance = balance + currentBalance
+  let miss = currentBalance == 0
+  let newMisses = if miss then (misses + 1) else 0
+  let newLastHitN = if miss then lastHitN else n
+  if (newMisses == 5) then (pure $ wallet currentBalance (lastHitN + 2)) else (fetchWallet_ newMisses newBalance newLastHitN (n + 1) key)
   where
-    receiveAddress = deriveAddress key n
-    currentWallet = { receiveAddress, balance }
+    currentAddress = deriveAddress key n
+    wallet bal nohitN = { receiveAddress: (deriveAddress key nohitN), balance: bal }
 
 balanceFromString ∷ String → Int
 balanceFromString str = case (Int.fromString str) of
